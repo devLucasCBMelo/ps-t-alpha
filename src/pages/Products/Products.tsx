444623858d827b66ff6b647246070f2f86f12df2
Lucas Costa <englucascbmelo@gmail.com>
2024-08-12 10:05:03 -0300

    inserção de botão que mostra/esconde os botões de excluir produtos

--- src/pages/Products/Products.tsx
+++ src/pages/Products/Products.tsx
@@ -8,9 +8,14 @@ function Products() {
   const [productsList, setProductsList] = useState<any[]>([]);
   const [productId, setProductId] = useState('');
   const [foundedProduct, setFoundedProduct] = useState<any>(null);
+  const [clicked, setClicked] = useState(false);
   const navigate = useNavigate();
 
   const {userToken} = useContext(context)
+
+  const hideDeleteButton = styles.card_plus_delete_button_container_invisible;
+  const showDeleteButton = styles.delete_button;
+
 
   const fetchAllProducts = async () => {
     try {
@@ -23,7 +28,7 @@ function Products() {
 
   useEffect(() => {
     fetchAllProducts();
-  }, [productsList]);
+  }, [productsList,clicked]);
 
   const findProductById = async () => {
     if (!productId) {
@@ -52,6 +57,16 @@ function Products() {
     }
   }
 
+  const changeVisibility = () => {
+    if (clicked) {
+      setClicked(false);
+
+    } else {
+      setClicked(true);
+
+    }
+  }
+
   return (
     <div className={styles.products_container}>
       <div className={styles.products_header}>
@@ -60,7 +75,8 @@ function Products() {
 
       <div>
         <button className={styles.register_button} onClick={() => navigate('/registerproducts')}>Cadastrar novo produto</button>
-        <button onClick={ () => navigate('/updateproducts')}>Atualizar produto</button>
+        <button className={ styles.update_button } onClick={ () => navigate('/updateproducts')}>Atualizar produto</button>
+        <button onClick={ () => changeVisibility() }>Deletar produto</button>
       </div>
 
       <div>
@@ -76,35 +92,49 @@ function Products() {
 
       <div className={styles.products_cards}>
         {foundedProduct ? (
-          <div className={styles.product_card}>
-            <div>
-              <p className={styles.card_id}><strong>id: </strong>{foundedProduct.id}</p>
-              <p className={styles.card_name}><strong>Nome: </strong>{foundedProduct.name}</p>
-              <p className={styles.card_description}><strong>Descrição: </strong>{foundedProduct.description}</p>
-              <p className={styles.card_quantity}><strong>Quantidade disponível no estoque: </strong>{foundedProduct.stock}</p>
-              <p className={styles.card_price}><strong>Preço: </strong>{foundedProduct.price}</p>
+          <div
+            className={ styles.card_plus_delete_button_container}
+          >
+            <button
+              className={ styles.delete_button }
+              onClick={ () => deleteProduct(foundedProduct.id)}
+            >
+              X
+            </button>
+            <div className={styles.product_card}>
+              <div>
+                <p className={styles.card_id}><strong>id: </strong>{foundedProduct.id}</p>
+                <p className={styles.card_name}><strong>Nome: </strong>{foundedProduct.name}</p>
+                <p className={styles.card_description}><strong>Descrição: </strong>{foundedProduct.description}</p>
+                <p className={styles.card_quantity}><strong>Quantidade no estoque: </strong>{foundedProduct.stock}</p>
+                <p className={styles.card_price}><strong>Preço: </strong>{foundedProduct.price}</p>
+              </div>
             </div>
           </div>
         ) : (
           productsList.map((product, index) => (
-            <div key={index} className={ styles.product_card_container}>
-              <div className={styles.product_card}>
-                <div>
-                  <p className={styles.card_id}><strong>id: </strong>{product.id}</p>
-                  <p className={styles.card_name}><strong>Nome: </strong>{product.name}</p>
-                  <p className={styles.card_description}><strong>Descrição: </strong>{product.description}</p>
-                  <p className={styles.card_quantity}><strong>Quantidade disponível no estoque: </strong>{product.stock}</p>
-                  <p className={styles.card_price}><strong>Preço: </strong>R$ {product.price}</p>
-                </div>
-              </div>
-
+            <div
+              className={ styles.card_plus_delete_button_container}
+              key={index}
+            >
               <button
-                className={ styles.delete_button }
+                className={ clicked ? showDeleteButton: hideDeleteButton }
                 onClick={ () => deleteProduct(product.id)}
               >
                 X
               </button>
+              <div className={ styles.product_card_container}>
+                <div className={styles.product_card}>
+                  <div>
+                    <p className={styles.card_id}><strong>id: </strong>{product.id}</p>
+                    <p className={styles.card_name}><strong>Nome: </strong>{product.name}</p>
+                    <p className={styles.card_description}><strong>Descrição: </strong>{product.description}</p>
+                    <p className={styles.card_quantity}><strong>Quantidade no estoque: </strong>{product.stock}</p>
+                    <p className={styles.card_price}><strong>Preço: </strong>R$ {product.price}</p>
+                  </div>
+                </div>
 
+              </div>
             </div>
           ))
         )}
